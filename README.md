c81de0a83513c6300a71695122dd7c19c5e86399
Lam <andrew.lam@fticonsulting.com>
2018-10-11 13:50:40 -0700

    Update directory names

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PyExtractPDF
-Python script to extract data from PDF files
+A Python script to extract text from PDF files 
+
+## Version History
+10/10/18 - Version 1.0 - Initial release
+
+## Usage
+* Copy OCRed PDF files to a subdirectory named "input".
+* An Excel file will be written to a subdirectory named "output".
--- pdfextract.py
+++ pdfextract.py
@@ -3,15 +3,15 @@ import glob
 import pandas as pd
 import re
 
-pageNum = 1
 output = []
-inputPDFList = glob.glob("./InputPDF/*.pdf")
+inputPDFList = glob.glob(".\input\*.pdf")
 
 # Loop through PDF files
 for inputPDF in inputPDFList:
     # Open PDF file
     pdfFile = PyPDF2.PdfFileReader(open(inputPDF, 'rb'))
     # Loop through pages
+    pageNum = 1
     while (pageNum <= pdfFile.numPages):
         
         # Get page of current page number
@@ -20,20 +20,30 @@ for inputPDF in inputPDFList:
         # Extract text from current page
         pdfText = pdfPage.extractText()
         
-        print(pdfText)
-        
         # Find ACCT_ID in text
         acct_id_match = re.search(r"\d{10}-\d", pdfText)
         if acct_id_match is not None:
-            acct_id = acct_id_match.group(0)
+            acct_id = acct_id_match.group(0)[:10]
+        else:
+            acct_id = ''
+            
+        # Find Statement Date
+        statement_date_match = re.search(r"\d{2}\/\d{2}\/\d{4} \d{2}\/\d{2}\/\d{4}", pdfText)
+        if statement_date_match is not None:
+            statement_date = statement_date_match.group(0)[:10]
+        else:
+            statement_date_match = re.search(r"Statement Date[:|;] \d{2}\/\d{2}\/\d{4}", pdfText)
+            if statement_date_match is not None:
+                statement_date = statement_date_match.group(0)[16:27]
+            else:
+                statement_date = ''
         
         # Output to list
-        output.append([inputPDF, pageNum, acct_id, pdfText])
+        output.append([inputPDF, pageNum, acct_id, statement_date, pdfText])
         pageNum += 1
 
 # Create dataframe and output to Excel File
-outputDF = pd.DataFrame(output, columns = ['Filename', 'PageNum', 'Acct_ID', 'Text'])
-
-writer = pd.ExcelWriter('output.xlsx')
+outputDF = pd.DataFrame(output, columns = ['FILENAME', 'PAGE_NUMBER', 'ACCT_ID', 'STATEMENT_DATE', 'TEXT'])
+writer = pd.ExcelWriter('./output/output.xlsx')
 outputDF.to_excel(writer,'Sheet1')
 writer.save()
